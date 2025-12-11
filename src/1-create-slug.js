89cea3047466bcd3bc6ac933bec6cd0a48f83a47
Felix Vargas <felix70529@gmail.com>
2025-12-11 13:55:24 -0500

    Completed coding section of swe-checkpoint

--- src/1-create-slug.js
+++ src/1-create-slug.js
@@ -1,9 +1,22 @@
 // Return a URL-friendly "slug": lowercase with hyphens instead of spaces.
 // Return null if the title contains banned characters: "!", "#", "?"
 const createSlug = (title) => {
-
+  if (title.includes("!") || title.includes("#") || title.includes("?")) {
+    return null;
+  }
+  const trimSlug = title.trim("");
+  const lowerCaseSlug = trimSlug.toLowerCase();
+  const partsSlug = lowerCaseSlug.split(" ");
+  const cleanSlug = [];
+  for (let i = 0; i < partsSlug.length; i++) {
+    if (partsSlug[i] !== "") {
+      cleanSlug.push(partsSlug[i]);
+    }
+  }
+  const formattedSlug = cleanSlug.join("-");
+  return formattedSlug;
 };
-
+console.log(createSlug("Hello World"));
 module.exports = {
   createSlug,
 };
--- src/2-user-profile.js
+++ src/2-user-profile.js
@@ -1,26 +1,40 @@
 // Return a new user object with username, email, isActive: true, loginCount: 0
 const createUser = (username, email) => {
-
+  user = {
+    username : username,
+    email : email,
+    isActive : true,
+    loginCount : 0
+  }
+  return user;
 };
-
 // Increase the user's loginCount by 1
 const incrementLogin = (user) => {
-
+  user.loginCount += 1;
 };
 
 // Set isActive to false and delete the email property. Return the user.
 const deactivateUser = (user) => {
-
+  user.isActive = false;
+  delete user.email;
+  return user;
 };
 
 // Print each property and value in the format "key: value"
 const printUserInfo = (user) => {
+  const keys = Object.keys(user);
 
+  for (let i = 0; i < keys.length; i++)  {
+    const key = keys[i]
+    const value = user[key]
+    console.log(`${key}: ${value}`);
+  }
 };
 
 // BONUS: Return a true copy of the user object (not a reference)
 const cloneUser = (user) => {
-
+  const userClone = { ...user}
+  return userClone;
 };
 
 module.exports = {
